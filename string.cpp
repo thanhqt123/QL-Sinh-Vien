fa34eb0fb23c31333fbf5a94d62e11e637e5361a
Thành <huuthanh9agm@gmail.com>
2024-04-05 01:47:25 +0700

    xap xep tu

--- string.cpp
+++ string.cpp
@@ -2,44 +2,36 @@
 #include <string>
 #include <sstream>
 using namespace std;
-
-int solve(string a[], int n) {
-    int cnt = 0;
-    bool appeared[10000] = {false}; // Mảng đánh dấu các từ đã xuất hiện, ban đầu chưa xuất hiện
-    for (int i = 0; i < n; ++i) {
-        if (!appeared[i]) {
-            appeared[i] = true; // Đánh dấu từ đã xuất hiện
-            ++cnt; // Tăng biến đếm lên 1
-            // Duyệt qua các từ sau và đánh dấu các từ trùng lặp
-            for (int j = i + 1; j < n; ++j) {
-                if (a[i] == a[j]) {
-                    appeared[j] = true;
-                }
+void tangDan(string a[], int n){
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i]>a[j]){
+                string tem=a[i];
+                a[i]=a[j];
+                a[j]=tem;
             }
         }
     }
-    return cnt;
 }
 
 int main() {
     string s;
-    getline(cin, s);
-
-    // Chuyển tất cả các ký tự thành chữ thường
-    for (char& c : s) {
-        c = tolower(c);
+    getline(std::cin, s);
+    stringstream ss (s);
+    int maxWords =10000;
+    string words[maxWords];
+    int wordCount=0;
+    string word;
+    while(ss>>word){
+        words[wordCount]=word;
+        wordCount++;
     }
-
-    stringstream ss(s);
-    const int wordMax = 10000;
-    string words[wordMax];
-    int wc = 0;
-    string w;
-
-    while (ss >> w && wc < wordMax) {
-        words[wc++] = w;
+    tangDan(words,wordCount);
+    for(int i=0;i<wordCount;i++){
+        cout<<words[i]<<" ";
     }
-
-    cout << solve(words, wc);
-    return 0;
+    cout<<"\n";
+    for(int i=wordCount-1;i>=0;i--){
+        cout<<words[i]<<" ";
+    }
 }
